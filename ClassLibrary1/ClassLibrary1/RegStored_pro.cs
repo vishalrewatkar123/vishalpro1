c4d91433c3e677bdd77a97d22fcb824b8e0f4e4a
vishalrewatkar123 <38560429+vishalrewatkar123@users.noreply.github.com>
2018-04-20 18:09:23 +0530

    Initial commit

---
+++ ClassLibrary1/ClassLibrary1/RegStored_pro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClassLibrary1
+{
+    
+public  class RegStored_pro
+ {
+  
+       
+        SqlConnection conn = new SqlConnection("Data Source=tss11;Initial Catalog=Vishal;User ID=sa;Password=sa123");
+
+        public void insert(String a, string b, string c)
+        {
+            SqlCommand cmd = new SqlCommand("v_InsertCUST", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", SqlDbType.VarChar).Value = a;
+            cmd.Parameters.AddWithValue("@Email", SqlDbType.VarChar).Value=b;
+            cmd.Parameters.AddWithValue("@Phone_No", SqlDbType.VarChar).Value = c;
+            conn.Open();
+            cmd.ExecuteNonQuery();
+           
+          
+        }
+}
+}
